6fe5eba8141b978c551498334c09b41a2fe1479b
devJasonClarke <jettison200@gmail.com>
2022-01-02 00:47:12 -0500

    ⚡first commit⚡

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <p data-testid="computed">
+      We have {{ spacesLeft }} out of {{ capacity }} seats
+    </p>
+    <h1>
+      Capacity: <span data-testid="capacity-updated-value">{{ capacity }}</span>
+    </h1>
+    <button data-testid="increase" @click="increaseCapacity()">Increase</button>
+    <button data-testid="decrease" @click="decreaseCapacity()">Decrease</button>
+
+    <div>
+      <ul v-for="(people, index) in attending" :key="index">
+        <li>{{ people }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { ref, computed } from "vue";
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  setup() {
+    const capacity = ref(3);
+    const attending = ref(["Jason", "Jon", "lou", "Kanye"]);
+    const spacesLeft = computed(() => {
+      return capacity.value - attending.value.length;
+    });
+    function increaseCapacity() {
+      capacity.value++;
+    }
+
+    function decreaseCapacity() {
+      capacity.value--;
+    }
+
+    return {
+      capacity,
+      attending,
+      spacesLeft,
+      increaseCapacity,
+      decreaseCapacity,
+    };
   },
 };
 </script>
+
+<style lang="scss" scoped></style>
---
+++ tests/unit/Home.spec.js
@@ -0,0 +1,60 @@
+import Home from "@/views/Home.vue";
+import { mount } from "@vue/test-utils";
+
+describe("Home.vue", () => {
+  test("Component exists", () => {
+    const wrapper = mount(Home);
+
+    expect(wrapper).toBeTruthy();
+  });
+
+  test("Wrapper contains word", async () => {
+    const wrapper = mount(Home);
+
+    const update = () => (wrapper.vm.capacity = 55);
+
+    await update();
+
+    expect(wrapper.text()).toContain(55);
+  });
+
+  test("increments a count when button is clicked", async () => {
+    const wrapper = mount(Home);
+
+    await wrapper.find("[data-testid='increase']").trigger("click");
+
+    expect(wrapper.find("[data-testid='capacity-updated-value']").text()).toBe(
+      "4"
+    );
+  });
+
+  test("decrements a count when button is clicked", async () => {
+    const wrapper = mount(Home);
+
+    await wrapper.find("[data-testid='decrease']").trigger("click");
+
+    expect(wrapper.find("[data-testid='capacity-updated-value']").text()).toBe(
+      "2"
+    );
+  });
+
+  test("test computed item's initial state", () => {
+    const wrapper = mount(Home);
+
+    // await wrapper.find("[data-testid='decrease']").trigger("click");
+
+    expect(wrapper.find("[data-testid='computed']").text()).toBe(
+      "We have -1 out of 3 seats"
+    );
+  });
+
+  test("test computed item is reactive", async () => {
+    const wrapper = mount(Home);
+
+    await wrapper.find("[data-testid='increase']").trigger("click");
+
+    expect(wrapper.find("[data-testid='computed']").text()).toBe(
+      "We have 0 out of 4 seats"
+    );
+  });
+});
